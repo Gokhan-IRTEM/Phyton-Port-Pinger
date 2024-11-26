2cabb8aea15ca3d863f6cf37a5a3690951704bc8
Gökhan İRTEM <107649196+Gokhan-IRTEM@users.noreply.github.com>
2024-11-26 19:27:01 +0300

    Update UDPPingerServer.py

--- Phyton Port Pinger/UDPPingerServer.py
+++ Phyton Port Pinger/UDPPingerServer.py
@@ -1,9 +1,7 @@
 # UDPPingerServer.py
-# We will need the following module to generate randomized lost packets
 import random
 from socket import *
 # Create a UDP socket
-# Notice the use of SOCK_DGRAM for UDP packets
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Assign IP address and port number to socket
 serverSocket.bind(('IP here', number here))
